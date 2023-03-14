830dd135304753802fcc5f0c02ff68d0bf5b3d37
redjax <jackenyon@gmail.com>
2023-03-14 15:51:48 -0400

    lua rewrite:
    Minimal working config

---
+++ nvim_config/lua/conf/init.lua
@@ -0,0 +1,7 @@
+-- # Init file to source lua/conf/*.lua configuration files
+
+require "conf.globals"
+require "conf.defaults"
+require "conf.environment"
+require "conf.mappings"
+-- require "conf.commands"
