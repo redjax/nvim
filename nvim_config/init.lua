3ed5b8198ac9091723c1aa4bda4d517a24392464
redjax <jackenyon@gmail.com>
2023-03-14 15:51:48 -0400

    lua rewrite:
    Minimal working config

--- nvim_config/init.lua
+++ nvim_config/init.lua
@@ -1,25 +1,11 @@
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.incsearch = true
+-- ## Main nvim configuration
+--  This file is written in Lua. For help, see:
+--    https://neovim.io/doc/user/lua-guide.html
+--
+--  Help can also be accessed with :help lua
 
-vim.o.hidden = true
-
-vim.o.history = 5000
-
-vim.o.tabstop = 4
-vim.o.shiftwidth = vim.o.tabstop
-vim.g.mapleader = ","
-
-local uname = vim.loop.os_uname()
-
-_G.OS = uname.sysname
-_G.IS_MAC = OS == 'Darwin'
-_G.IS_LINUX = OS == 'Linux'
-_G.IS_WINDOWS = OS:find 'Windows' and true or false
-_G.IS_WSL = IS_LINUX and uname.release:find 'Microsoft' and true or false
-
-print(_G.OS)
--- require "paq" {
---     "savq/paq-nvim"
--- }
--- vim.fn.has('macunix')
+-- # Config imports
+--   require syntax uses 'lua' dir as starting point. For nested files/dirs,
+--     use dot notation, i.e. conf.some-file.lua
+require "conf"
+-- require "plugins"
