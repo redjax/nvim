e4114415e1b2687e50827424a6e5be24a4d27b51
redjax <jackenyon@gmail.com>
2023-03-15 17:23:00 -0400

    Lua conf:
    Add/update Lua configs.
    
    Move key map function to utils.lua, out of globals.

--- nvim_config/lua/conf/autocmds.lua
+++ nvim_config/lua/conf/autocmds.lua
@@ -2,43 +2,45 @@
 local cmd = vim.cmd
 local api = vim.api
 
-
 -- Highlight on yank
-cmd [[
-    augroup YankHighlight
-        autocmd!
-        autocmd TextYankPost * silent! lua vim.highlight.on_yank()
-]]
+local highlight = api.nvim_create_augroup("YankHighlight", { clear = true })
+api.nvim_create_autocmd("TextYankPost", {
+    pattern = "*",
+    group = highlight,
+    desc = "Highlight yanked text",
+    command = 'silent! lua vim.highlight.on_yank()'
+})
 
 -- This PC is fast enough, do syntax highlighting
--- synching from start, unless > 200 lines
--- cmd [[
---     augroup vimrc-sync-fromstart
---         autocmd!
---         autocmd BufEnter * :syntax sync maxlines=200
---     augroup end
--- ]]
 local sync_fromstart = api.nvim_create_augroup("vimrc_sync_fromstart", { clear = true })
-vim.api.nvim_create_autocmd("BufEnter", {
+api.nvim_create_autocmd("BufEnter", {
     pattern = "*",
     group = sync_fromstart,
     desc = "Speed up loading of nvim config files",
-    command = "syntax sync maxlines=200"
+    command = "syntax sync maxlines=1000"
 })
 
 -- Remember cursor position
-cmd [[
-    augroup vimrc-remember-cursor-position
-        autocmd!
-        autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
-    augroup end
-]]
+local remember_cursor_position = api.nvim_create_augroup("vimrc_remember_cursor_position", { clear = true })
+api.nvim_create_autocmd("BufReadPost", {
+    pattern = "*",
+    group = remember_cursor_position,
+    desc = "Remember cursor position from last time file was edited",
+    -- Run vimscript commands using [[ double brackets ]]
+    command = [[ if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif ]]
+})
 
 -- Make/CMake
-cmd [[
-    augroup vimrc-make-cmake
-        autocmd!
-        autocmd FileType make setlocal noexpandtab
-        autocmd BufNewFile, BufRead CMakeLists.txt setlocal filetype=cmake
-    augroup end
-]]
+local vimrc_make_cmake = api.nvim_create_augroup("vimrc_make_cmake", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "make",
+    group = vimrc_make_cmake,
+    desc = "Set noexpandtab on make",
+    command = "setlocal noexpandtab"
+})
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+    pattern = "CMakeLists.txt",
+    group = vimrc_make_cmake,
+    desc = "Set filetype to cmake",
+    command = "setLocal filetype=cmake"
+})
