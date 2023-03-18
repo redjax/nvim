352d8801e18847731ccf61aa58c0c1b5b2e9f8d9
redjax <jackenyon@gmail.com>
2023-03-18 02:06:38 -0400

    Nvim opens, but settings aren't where I want them

--- nvim_config/lua/general/init.lua
+++ nvim_config/lua/general/init.lua
@@ -1,4 +1,6 @@
-require 'general.autocmds'
-require 'general.functions'
-require 'general.keybinds'
-require 'general.options'
+require "general.reloads"
+require "general.options"
+require "general.autocmds"
+require "general.variables"
+require "general.keybinds"
+require "general.functions"
--- nvim_config/lua/utils/option.lua
+++ nvim_config/lua/utils/option.lua
@@ -1,48 +1,44 @@
 local Type = {
-    GLOBAL_OPTION = "o",
-    WINDOW_OPTION = "wo",
-    BUFFER_OPTION = "bo"
+	GLOBAL_OPTION = "o",
+	WINDOW_OPTION = "wo",
+	BUFFER_OPTION = "bo",
 }
 
 local add_options = function(option_type, id, options)
-    -- If options are of type "table"
-    if type(id) == "table" then
-        options = id
-        id = 0
-    end
+	if type(id) == "table" then
+		options = id
+		id = 0
+	end
 
     -- If options are not of type "table"
-    if type(options) ~= "table" then
-        error('Options should be of type "table"')
-        return
-    end
+	if type(options) ~= "table" then
+		error('options should be a type of "table"')
+		return
+	end
 
-    for key, value in pairs(options) do
-        -- Add options to Nvim. ID condition exists to make it compatible with global options
+	for key, value in pairs(options) do
+		-- Add options to Nvim. ID condition exists to make it compatible with global options
         --   Global options have no ID.
-        if id == 0 then
-            vim[option_key][key] = value
-        else
-            vim[option_type][id][key] = value
-        end
-    end
+		if id == 0 then
+			vim[option_type][key] = value
+		else
+			vim[option_type][id][key] = value
+		end
+	end
 end
 
 local Option = {}
 
--- Set a global option
 Option.g = function(options)
-    add_options(Type.GLOBAL_OPTION, options)
+	add_options(Type.GLOBAL_OPTION, options)
 end
 
--- Set a window option
 Option.w = function(id, options)
-    add_options(Type.WINDOW_OPTION, id, options)
+	add_options(Type.WINDOW_OPTION, id, options)
 end
 
--- Set a buffer option
 Option.b = function(id, options)
-    add_options(Type.BUFFER_OPTION, id, options)
+	add_options(Type.BUFFER_OPTION, id, options)
 end
 
 return Option
