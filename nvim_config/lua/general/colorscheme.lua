50e2c13ade5c1fba2ca7862427aeca4128480cf2
Jack <redjax@users.noreply.github.com>
2023-03-19 01:24:36 -0400

    Revert "Feat/rc release"

--- nvim_config/lua/general/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-Option.g({
-    bg = "dark",
-    -- Check the colors/ dir at the nvim config root for options
-    colorscheme = vim.cmd [[
-        colorscheme PaperColor
-    ]],
-})
--- nvim_config/lua/plugins/git-signs/commands.lua
+++
@@ -1,5 +0,0 @@
-vim.api.nvim_create_user_command("BlameCurrentLine", function()
-    require("gitsigns").toggle_current_line_blame()
-end,
-
-{ nargs = 0 })
