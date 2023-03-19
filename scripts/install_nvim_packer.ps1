2f2c48d698534bcc150d44a907aef3f84c71946d
redjax <jackenyon@gmail.com>
2023-03-19 01:15:43 -0400

    Remove scripts

--- scripts/install_nvim_packer.ps1
+++
@@ -1,6 +0,0 @@
-
-Write-Host""
-Write-Host "Installing Nvim Packer plugin manager"
-Write-Host ""
-
-git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
