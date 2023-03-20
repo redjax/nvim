ad2e3c3f9fc26e75321a11f435ff6208f838753a
Jack Kenyon <jkenyon@embracepetinsurance.com>
2023-03-20 08:42:58 -0400

    Add scripts back

---
+++ scripts/install_nvim_packer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo ""
+echo "Installing Neovim Packer plugin manager"
+echo ""
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
