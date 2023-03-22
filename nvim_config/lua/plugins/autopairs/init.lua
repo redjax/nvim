1e1adfb9446e5f48095cfa385d0c11a992581fa1
Jack Kenyon <jkenyon@embracepetinsurance.com>
2023-03-22 13:10:49 -0400

    Feat: env detection
    Update plugin loading, check environment first and only load certain plugins on Windows

--- nvim_config/lua/plugins/autopairs/init.lua
+++ nvim_config/lua/plugins/autopairs/init.lua
@@ -1,1 +1,8 @@
-require "plugins.autopairs.settings"
+-- Define host environment
+local uname = vim.loop.os_uname()
+-- Set local env var for OS
+local _OS = uname.sysname
+
+if _OS == "Linux" then
+    require "plugins.autopairs.settings"
+end
